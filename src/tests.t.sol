17c5a6312b0af795b6ec93a2a41e74004befc384
David Terry <me@xwvvvvwx.com>
2021-05-31 11:40:39 +0200

    try to run the deployers

--- src/tests.t.sol
+++ src/tests.t.sol
@@ -47,6 +47,27 @@ contract Test is DSTest {
             address(new AssessorAdminFab())
         );
 
+        // TODO: set these to sensible values...
+        lenderDeployer.init(
+            0, // minSeniorRatio
+            1, // maxSeniorRatio
+            1000000 ether, //maxReserve
+            1 days, // challengeTime
+            1, // seniorInterestRate
+            "Tin", // seniorName
+            "TIN", // seniorSymbol
+            "DROP", // juniorName
+            "DRP" // juniorSymbol
+        );
+
+        lenderDeployer.deployJunior();
+        lenderDeployer.deploySenior();
+        lenderDeployer.deployReserve();
+        lenderDeployer.deployAssessor();
+        lenderDeployer.deployAssessorAdmin();
+        lenderDeployer.deployCoordinator();
+        lenderDeployer.deploy();
+
         borrowerDeployer = new BorrowerDeployer(
             address(root),
             address(new TitleFab()),
@@ -59,5 +80,19 @@ contract Test is DSTest {
             "TTL",
             0 // TODO: wtf is a discount rate?
         );
+
+        borrowerDeployer.deployCollector();
+        borrowerDeployer.deployPile();
+        borrowerDeployer.deployTitle();
+        borrowerDeployer.deployShelf();
+        borrowerDeployer.deployFeed();
+        borrowerDeployer.deploy();
+
+        root.prepare(address(lenderDeployer), address(borrowerDeployer), address(this));
+        root.deploy();
+    }
+
+    function test_nothing() public {
+        assertTrue(true);
     }
 }
