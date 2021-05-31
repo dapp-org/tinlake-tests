8f90f5d60531c75e36a9db894cde228156df1b81
David Terry <me@xwvvvvwx.com>
2021-05-31 16:48:51 +0200

    deploy maker

--- src/tests.t.sol
+++ src/tests.t.sol
@@ -22,21 +22,82 @@ import {PileFab} from "tinlake/borrower/fabs/pile.sol";
 import {CollectorFab} from "tinlake/borrower/fabs/collector.sol";
 import {NAVFeedFab} from "tinlake/borrower/fabs/navfeed.sol";
 
+import {
+    DssDeploy,
+    VatFab,
+    JugFab,
+    VowFab,
+    CatFab,
+    DogFab,
+    DaiFab,
+    DaiJoinFab,
+    FlapFab,
+    FlopFab,
+    FlipFab,
+    ClipFab,
+    SpotFab,
+    PotFab,
+    EndFab,
+    ESMFab,
+    PauseFab
+} from "dss-deploy/DssDeploy.sol";
+
 contract Test is DSTest {
     TinlakeRoot root;
     LenderDeployer lenderDeployer;
     BorrowerDeployer borrowerDeployer;
-    Dai dai;
+    DssDeploy dssDeploy;
+
+    bytes32 constant ilkId = bytes32("DROP-A");
 
     function setUp() public {
-        uint chainId = 1;
 
-        dai = new Dai(chainId);
+        // -- deploy mcd --
+
+        uint chainId = 1;
+        uint pauseDelay = 0;
+        uint esmThreshold = 0;
+        address gov = address(this);
+
+        dssDeploy = new DssDeploy();
+        dssDeploy.addFabs1(
+            new VatFab(),
+            new JugFab(),
+            new VowFab(),
+            new CatFab(),
+            new DogFab(),
+            new DaiFab(),
+            new DaiJoinFab()
+        );
+        dssDeploy.addFabs2(
+            new FlapFab(),
+            new FlopFab(),
+            new FlipFab(),
+            new ClipFab(),
+            new SpotFab(),
+            new PotFab(),
+            new EndFab(),
+            new ESMFab(),
+            new PauseFab()
+        );
+
+        dssDeploy.deployVat();
+        dssDeploy.deployDai(chainId);
+        dssDeploy.deployTaxation();
+        dssDeploy.deployAuctions(gov);
+        dssDeploy.deployLiquidator();
+        dssDeploy.deployEnd();
+        dssDeploy.deployPause(pauseDelay, gov);
+        dssDeploy.deployESM(gov, esmThreshold);
+        dssDeploy.releaseAuth();
+
+        // -- deploy tinlake --
+
         root = new TinlakeRoot(address(this));
 
         lenderDeployer = new LenderDeployer(
             address(root),
-            address(dai),
+            address(dssDeploy.dai()),
             address(new TrancheFab()),
             address(new MemberlistFab()),
             address(new RestrictedTokenFab()),
@@ -49,15 +110,15 @@ contract Test is DSTest {
 
         // TODO: set these to sensible values...
         lenderDeployer.init(
-            0, // minSeniorRatio
-            1, // maxSeniorRatio
-            1000000 ether, //maxReserve
-            1 days, // challengeTime
-            1, // seniorInterestRate
-            "Tin", // seniorName
-            "TIN", // seniorSymbol
-            "DROP", // juniorName
-            "DRP" // juniorSymbol
+            0,             // minSeniorRatio
+            1,             // maxSeniorRatio
+            1000000 ether, // maxReserve
+            1 days,        // challengeTime
+            1,             // seniorInterestRate
+            "Drop",        // seniorName
+            "DRP",         // seniorSymbol
+            "Tin",         // juniorName
+            "TIN"          // juniorSymbol
         );
 
         lenderDeployer.deployJunior();
@@ -75,7 +136,7 @@ contract Test is DSTest {
             address(new PileFab()),
             address(new CollectorFab()),
             address(new NAVFeedFab()),
-            address(dai),
+            address(dssDeploy.dai()),
             "title",
             "TTL",
             0 // TODO: wtf is a discount rate?
@@ -90,6 +151,13 @@ contract Test is DSTest {
 
         root.prepare(address(lenderDeployer), address(borrowerDeployer), address(this));
         root.deploy();
+
+        // -- add drop as a collateral type --
+
+        //dss.deployCollateralFlip(ilkId, address join, address pip);
+        //dss.deployCollateralClip(ilkId, address join, address pip, address calc);
+        //releaseAuthFlip(bytes32 ilk);
+        //releaseAuthClip(bytes32 ilk);
     }
 
     function test_nothing() public {
