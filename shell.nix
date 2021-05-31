020822f9f18a1a3400967bf0057da51920511cb8
David Terry <me@xwvvvvwx.com>
2021-05-31 09:58:44 +0200

    nixification

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.dapptools {};
+in
+  pkgs.mkShell {
+    buildInputs = with pkgs; [
+      dapp
+      seth
+      hevm
+      niv
+      solc-static-versions.solc_0_6_12
+    ];
+    DAPP_SOLC="solc-0.6.12";
+  }
