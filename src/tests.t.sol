7022eecad56a621a195082105993b3108b13f3cc
Martin Lundfall <martin.lundfall@protonmail.com>
2021-06-01 18:04:20 +0200

    set up suitable for investing tests

--- src/tests.t.sol
+++ src/tests.t.sol
@@ -2,25 +2,29 @@
 pragma solidity ^0.6.12;
 
 import {DSTest} from "ds-test/test.sol";
+import {Math} from "tinlake-math/math.sol";
 import {Dai} from "dss/dai.sol";
 import {TinlakeRoot} from "tinlake/root.sol";
 import {LenderDeployer} from "tinlake/lender/deployer.sol";
 import {BorrowerDeployer} from "tinlake/borrower/deployer.sol";
 
 import {TrancheFab} from "tinlake/lender/fabs/tranche.sol";
-import {MemberlistFab} from "tinlake/lender/fabs/memberlist.sol";
+import {MemberlistFab, Memberlist} from "tinlake/lender/fabs/memberlist.sol";
 import {RestrictedTokenFab} from "tinlake/lender/fabs/restrictedtoken.sol";
-import {ReserveFab} from "tinlake/lender/fabs/reserve.sol";
+import {ReserveFab, Reserve} from "tinlake/lender/fabs/reserve.sol";
 import {AssessorFab} from "tinlake/lender/fabs/assessor.sol";
-import {CoordinatorFab} from "tinlake/lender/fabs/coordinator.sol";
+import {CoordinatorFab, EpochCoordinator} from "tinlake/lender/fabs/coordinator.sol";
 import {OperatorFab} from "tinlake/lender/fabs/operator.sol";
 import {AssessorAdminFab} from "tinlake/lender/fabs/assessoradmin.sol";
 
-import {TitleFab} from "tinlake/borrower/fabs/title.sol";
+import {TitleFab, Title} from "tinlake/borrower/fabs/title.sol";
 import {ShelfFab} from "tinlake/borrower/fabs/shelf.sol";
-import {PileFab} from "tinlake/borrower/fabs/pile.sol";
+import {PileFab, Pile} from "tinlake/borrower/fabs/pile.sol";
 import {CollectorFab} from "tinlake/borrower/fabs/collector.sol";
-import {NAVFeedFab} from "tinlake/borrower/fabs/navfeed.sol";
+import {NAVFeedFab, NAVFeed} from "tinlake/borrower/fabs/navfeed.sol";
+
+import {Borrower} from "tinlake/test/system/users/borrower.sol";
+import {Investor} from "tinlake/test/system/users/investor.sol";
 
 import {
     DssDeploy,
@@ -50,19 +54,44 @@ interface Hevm {
     function addr(uint) external returns (address);
 }
 
-contract Test is DSTest {
+contract Test is DSTest, Math {
     TinlakeRoot root;
     LenderDeployer lenderDeployer;
     BorrowerDeployer borrowerDeployer;
     DssDeploy dssDeploy;
     Hevm hevm = Hevm(HEVM_ADDRESS);
+    address dai;
+
+    Dai drop;
+    Dai tin;
+
+    Title public collateralNFT = new Title("Collateral NFT", "collateralNFT");
+
+    uint constant public DEFAULT_NFT_PRICE = 100000000 ether;
+    uint constant public DEFAULT_RISK_GROUP_TEST_LOANS = 3;
+    uint constant public DEFAULT_SENIOR_RATIO = 82 * 10**25;
+    uint constant public DEFAULT_JUNIOR_RATIO = 18 * 10**25;
+    uint loan;
+
+    // users
+    Borrower borrower;
+    Investor seniorInvestorA;
+    Investor seniorInvestorB;
+    Investor juniorInvestorA;
+    Investor juniorInvestorB;
+
+    Memberlist srMemberList;
+    Memberlist jrMemberList;
+    NAVFeed feed;
+    Pile pile;
+    Reserve reserve;
+    EpochCoordinator coordinator;
 
     bytes32 constant ilkId = bytes32("DROP-A");
 
     function setUp() public {
 
         // -- deploy mcd --
-
         uint chainId = 1;
         uint pauseDelay = 0;
         uint esmThreshold = 0;
@@ -124,13 +153,13 @@ contract Test is DSTest {
             address(new AssessorAdminFab())
         );
 
-        // TODO: set these to sensible values...
+        // values set according to config.sol
         lenderDeployer.init(
-            0,             // minSeniorRatio
-            1,             // maxSeniorRatio
+            0.75 *10**27,  // minSeniorRatio
+            0.85 *10**27,  // maxSeniorRatio
             1000000 ether, // maxReserve
-            1 days,        // challengeTime
-            1,             // seniorInterestRate
+            1 hours,        // challengeTime
+            uint(1000000229200000000000000000), // seniorInterestRate (2% / day)
             "Drop",        // seniorName
             "DRP",         // seniorSymbol
             "Tin",         // juniorName
@@ -145,6 +174,10 @@ contract Test is DSTest {
         lenderDeployer.deployCoordinator();
         lenderDeployer.deploy();
 
+        dai = address(dssDeploy.dai());
+        coordinator = EpochCoordinator(lenderDeployer.coordinator());
+        reserve = Reserve(lenderDeployer.reserve());
+
         borrowerDeployer = new BorrowerDeployer(
             address(root),
             address(new TitleFab()),
@@ -152,10 +185,11 @@ contract Test is DSTest {
             address(new PileFab()),
             address(new CollectorFab()),
             address(new NAVFeedFab()),
-            address(dssDeploy.dai()),
+            dai,
             "title",
             "TTL",
-            0 // TODO: wtf is a discount rate?
+            // discountRate 3% per day
+            uint(1000000342100000000000000000)
         );
 
         borrowerDeployer.deployTitle();
@@ -164,6 +198,8 @@ contract Test is DSTest {
         borrowerDeployer.deployShelf();
         borrowerDeployer.deployCollector();
         borrowerDeployer.deploy();
+
+        pile = Pile(borrowerDeployer.pile());
 
         root.prepare(address(lenderDeployer), address(borrowerDeployer), address(this));
         root.deploy();
@@ -174,13 +210,148 @@ contract Test is DSTest {
         //dss.deployCollateralClip(ilkId, address join, address pip, address calc);
         //releaseAuthFlip(bytes32 ilk);
         //releaseAuthClip(bytes32 ilk);
+
+
+        // -- create borrower user
+        borrower = new Borrower(borrowerDeployer.shelf(),
+                                address(reserve),
+                                borrowerDeployer.currency(),
+                                borrowerDeployer.pile());
+
+        // set up and price the nft collateral
+        uint tokenId = collateralNFT.issue(address(borrower));
+        feed = NAVFeed(borrowerDeployer.feed());
+        priceNFTandSetRisk(tokenId, DEFAULT_NFT_PRICE, DEFAULT_RISK_GROUP_TEST_LOANS);
+
+        // enable it for loans
+        borrower.approveNFT(collateralNFT, borrowerDeployer.shelf());
+        loan = borrower.issue(address(collateralNFT), tokenId);
+
+
+        // -- create new investor users
+        drop = Dai(lenderDeployer.seniorToken());
+        tin  = Dai(lenderDeployer.juniorToken());
+        seniorInvestorA = new Investor(lenderDeployer.seniorOperator(),
+                                       lenderDeployer.seniorTranche(),
+                                       address(dai),
+                                       address(drop));
+
+        seniorInvestorB = new Investor(lenderDeployer.seniorOperator(),
+                                       lenderDeployer.seniorTranche(),
+                                       address(dai),
+                                       address(drop));
+
+        juniorInvestorA = new Investor(lenderDeployer.juniorOperator(),
+                                       lenderDeployer.juniorTranche(),
+                                       address(dai),
+                                       address(tin));
+
+        juniorInvestorB = new Investor(lenderDeployer.juniorOperator(),
+                                       lenderDeployer.juniorTranche(),
+                                       address(dai),
+                                       address(tin));
+        
+        // -- authorize this contract on the whitelist contracts
+        srMemberList = Memberlist(lenderDeployer.seniorMemberlist());
+        jrMemberList = Memberlist(lenderDeployer.juniorMemberlist());
+        root.relyContract(address(srMemberList), address(this));
+        root.relyContract(address(jrMemberList), address(this));
+        KYC(address(seniorInvestorA));
+        KYC(address(juniorInvestorA));
     }
 
-    function test_nothing() public {
-        assertTrue(true);
-        Dai dai = dssDeploy.dai();
-        assertEq(dai.totalSupply(), 0);
-        dai.mint(address(this), type(uint).max);
-        assertEq(dai.totalSupply(), type(uint).max);
+    function testInvestmentsReturns(uint32 amount) public {
+        if (amount * 1 ether > feed.ceiling(loan)) return;
+          investBothTranches(amount * 1 ether);
+          // close epoch
+          hevm.warp(block.timestamp + 1 days);
+          coordinator.closeEpoch();
+
+          // 1 wei can be lost due to rounding errs.
+          uint available = reserve.currencyAvailable();
+          assertLe(amount * 1 ether - available, 1);
+          // borrow all of it
+          borrower.borrowAction(loan, available);
+          // accumulate debt
+          hevm.warp(block.timestamp + 1 days);
+          // interest is 5% a DAY!
+          uint debt = pile.debt(loan);
+
+          // at least within 1 wei of margin
+          assertLe(debt - (available * 105) / 100, 1);
+          
+          // give borrower some dai so they can repay
+          Dai(dai).mint(address(borrower), debt - available);
+          borrower.doApproveCurrency(borrowerDeployer.shelf(), type(uint).max);
+          borrower.repayAction(loan, debt);
+
+          // investor exits completely
+          seniorInvestorA.disburse();
+          juniorInvestorA.disburse();
+
+          // TODO: why can they not redeem completely? (coordinator won't execute epoch directly)
+          /* seniorInvestorA.redeemOrder(drop.balanceOf(address(seniorInvestorA))); */
+          /* juniorInvestorA.redeemOrder( tin.balanceOf(address(juniorInvestorA))); */
+
+          // redeem half
+          seniorInvestorA.redeemOrder(drop.balanceOf(address(seniorInvestorA)) / 2);
+          juniorInvestorA.redeemOrder( tin.balanceOf(address(juniorInvestorA)) / 2);
+
+          hevm.warp(block.timestamp + 1 days);
+
+          coordinator.closeEpoch();
+          assertTrue(!coordinator.submissionPeriod());
+
+          seniorInvestorA.disburse();
+
+          juniorInvestorA.disburse();
+          
+          // did he get his money back?
+          uint got = Dai(dai).balanceOf(address(seniorInvestorA));
+          log_named_uint("sr investor A put in    ", rmul(amount * 1 ether, DEFAULT_SENIOR_RATIO));
+          log_named_uint("withdrawing half yielded", got);
+          uint expected = rmul(amount * 1 ether, DEFAULT_SENIOR_RATIO) * 102 / 100 / 2;
+          if (expected == 0) return;
+          assertLe(expected, got); // we always make more than we expect, somehow
+          assertLe(((got - expected) * 100) / expected, 1);
+    }
+
+    
+    function priceNFTandSetRisk(uint tokenId, uint nftPrice, uint riskGroup) public {
+        uint maturityDate = 600 days;
+        bytes32 lookupId = keccak256(abi.encodePacked(address(collateralNFT), tokenId));
+
+        // -- authorize this contract
+        root.relyContract(address(feed), address(this));
+
+        // -- set price and risk
+        feed.update(lookupId, nftPrice, riskGroup);
+        // add default maturity date
+        feed.file("maturityDate", lookupId , maturityDate);
+    }
+
+    function KYC(address usr) public {
+        uint validUntil = block.timestamp + 200 days;
+        jrMemberList.updateMember(usr, validUntil);
+        srMemberList.updateMember(usr, validUntil);
+    }
+
+    function investBothTranches(uint currencyAmount) public {
+        uint amountSenior = rmul(currencyAmount, DEFAULT_SENIOR_RATIO);
+        uint amountJunior = rmul(currencyAmount, DEFAULT_JUNIOR_RATIO);
+
+        Dai(dai).mint(address(seniorInvestorA), amountSenior);
+        Dai(dai).mint(address(juniorInvestorA), amountJunior);
+
+        seniorInvestorA.supplyOrder(amountSenior);
+        juniorInvestorA.supplyOrder(amountJunior);
     }
 }
+
+
+// 3 investors put in x_i dai
+// a borrower borrows
+// wait some time
+// they repay their loan
+// THERE SHOULD BE NO WAY FOR investor 0 to get more than
+// x_0 * 1.03
